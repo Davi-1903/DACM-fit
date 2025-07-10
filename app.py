89055fae4c0995cc0f6a7654b52d88aaf11b6af3
Davi <franciscodavi327@gmail.com>
2025-07-10 08:06:39 -0300

    Adicionar templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
@@ -6,7 +6,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/logout') # Adicionar login_required
+def logout():
+    ...
 
 
 if __name__ == '__main__':
