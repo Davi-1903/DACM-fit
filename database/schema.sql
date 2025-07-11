1348a6de15dc0742158eb4b77d34870599f77482
Davi <franciscodavi327@gmail.com>
2025-07-11 15:37:14 -0300

    Corrigir e melhorar schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS tb_usuario(
-    usu_id int auto_increment primary key not null,
+    usu_id int autoincrement primary key not null,
     usu_nome varchar(100) not null,
-    usu_email varchar(100) not null,
+    usu_email varchar(100) not null unique,
     usu_senha varchar(200) not null -- O tamanho do hash é 162, mas 200 é um número mais redondo :)
 );
