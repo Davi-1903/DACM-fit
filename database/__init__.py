d11c0f2aaa3c1b69be4be9cbc7a1bf7c88153d3b
Maria <garciamariadasgracas318@gmail.com>
2025-08-02 13:24:37 -0300

    Criar opção para select all e corrigir a criação do database.

--- database/__init__.py
+++ database/__init__.py
@@ -4,4 +4,4 @@ import sqlite3
 def init_database():
     with sqlite3.connect('database/banco.db') as conn:
         with open('database/schema.sql') as f:
-            conn.execute(f.read())
+            conn.executescript(f.read())
--- utils.py
+++ utils.py
@@ -7,7 +7,15 @@ FILE = 'database/banco.db'
 def script_sql(script: str, values: tuple = ()):
     with sqlite3.connect(FILE) as conn:
         conn.row_factory = sqlite3.Row
+        conn.execute('PRAGMA foreign_keys = ON')
         if 'select' in script.lower():
             return conn.execute(script, values).fetchone()
         conn.execute(script, values)
         conn.commit()
+
+def script_sql_all(script: str, values: tuple = ()):
+    with sqlite3.connect(FILE) as conn:
+        conn.row_factory = sqlite3.Row
+        conn.execute('PRAGMA foreign_keys = ON')
+        return conn.execute(script, values).fetchall()
+
