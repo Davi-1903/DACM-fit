870c5197258e972d1984de79181221cf5e1dc1b8
Maria <garciamariadasgracas318@gmail.com>
2025-08-02 13:25:08 -0300

    Criar tabela para registros de treino dos usuários.

--- database/schema.sql
+++ database/schema.sql
@@ -1,5 +1,7 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE IF NOT EXISTS tb_usuario(
-    usu_id int auto_increment primary key not null,
+    usu_id integer primary key autoincrement not null,
     usu_nome varchar(100) not null,
     usu_email varchar(100) not null unique,
     usu_senha varchar(200) not null, -- O tamanho do hash é 162, mas 200 é um número mais redondo :)
@@ -10,4 +12,14 @@ CREATE TABLE IF NOT EXISTS tb_usuario(
     usu_peso float,
     usu_altura float,
     usu_tipo_treino varchar(20)
+);
+
+CREATE TABLE IF NOT EXISTS tb_registro_treino(
+    reg_id integer primary key autoincrement not null,
+    reg_treinou varchar(3) not null,
+    reg_data date,
+    reg_tempo integer,
+    reg_observacao text,
+    reg_usu_id integer not null,
+    foreign key (reg_usu_id) references tb_usuario(usu_id)
 );
